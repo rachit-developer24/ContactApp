97c4e944d5bd60b858f3305b994139750f8a08cf
rachit-developer24 <srachit841@gmail.com>
2025-07-20 17:01:55 +0100

    Initial Commit

---
+++ ContactApp/ContactAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactAppApp.swift
+//  ContactApp
+//
+//  Created by Rachit Sharma on 20/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ContactAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContactAppTests/ContactAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ContactAppTests.swift
+//  ContactAppTests
+//
+//  Created by Rachit Sharma on 20/07/2025.
+//
+
+import Testing
+@testable import ContactApp
+
+struct ContactAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
